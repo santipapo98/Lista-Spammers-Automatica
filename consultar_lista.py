847461fa985b86814d16dfe0a0b056adee0d035b
Santiago <santipapo98@gmail.com>
2021-03-14 18:16:28 -0500

    Primera version, funciona ingresando la IP Manual

--- consultar_lista.py
+++ consultar_lista.py
@@ -1,4 +1,5 @@
 import selenium 
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -17,14 +18,33 @@ driver.get("https://mxtoolbox.com/blacklists.aspx")
 
 search = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucToolhandler_txtToolInput")
 
-search.send_keys("173.22.13.223")
+direccion_IP = "184.22.13.223"
+
+search.send_keys(direccion_IP)
 
 search.send_keys(Keys.RETURN)
 
-time.sleep(3)
-
 numero = driver.find_elements_by_xpath('/html/body/form/div[3]/div[2]/div[2]/div/div[2]/div[2]/span/div/div[3]/strong[3]')
 
-print(numero[0].text)
+cantidad_veces = numero[0].text
+
+if int(cantidad_veces) > 0:
+#Buscar la direccion en Geotool
+    driver.get("https://iplookup.flagfox.net/")
+    search = driver.find_element_by_id('ipaddress')
+    search.send_keys(direccion_IP)
+    search.send_keys(Keys.RETURN)
+
+#Recolectar los datos de la busqueda
+    pais = driver.find_element_by_xpath('/html/body/div[1]/div[2]/table/tbody/tr[3]/td[2]/span/a/b')
+    ciudad = driver.find_element_by_xpath('/html/body/div[1]/div[2]/table/tbody/tr[5]/td[2]')
+    isp = driver.find_element_by_xpath('/html/body/div[1]/div[2]/table/tbody/tr[1]/td[5]')
+#Ingresar los datos al Excel    
+    df = pd.read_excel('Lista_Spammers.xlsx',index_col=0)
+    df = df.append({'IP' : direccion_IP, 'pais': pais.text, 'ciudad' : ciudad.text, 'isp' : isp.text, 'veces_detectado' : cantidad_veces}, ignore_index=True)
+    df.to_excel('Lista_Spammers.xlsx')
+
+else:
+    print('El numero NO es mayor a Cero, care verga')
 
 driver.quit()
